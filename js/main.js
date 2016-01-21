acbfc3206abf410078a7b33e74f0c64beaf441ba
nesterone <igornester@gmail.com>
2016-01-21 16:03:42 +0200

    #7 Provide few demos in github repo and component to display them through jsfiddle viewer/editor -make sidebar to work in sync with router

--- js/main.js
+++ js/main.js
@@ -6,29 +6,27 @@ $(function(){
 
     'use strict';
 
-    function showDemo(demoId){
-        console.log(demoId);
+    function SideBar(){
+
+        var self = this;
+        self.$el = $('.dl-list');
+
+        self.$el.find('.dl-list__item').on('click', function(){
+             var itemId = $(this).attr('data-id');
+             self.selectItem(itemId);
+        });
     }
 
-    var router = Router({
-        '/demos/:demoId': showDemo
-    });
+    SideBar.prototype.selectItem = function(itemId){
 
-    router.init("demos/report-basic-render");
+        var $item = this.$el.find("[data-id=" + itemId + "]");
 
-    $('.dl-list').on('click', function(evt){
-        var $target = $(evt.target);
-        if ($target.is('a')){
-
-            $(this)
-                .find('.dl-list__item--selected')
+        this.$el.find('.dl-list__item--selected')
                 .removeClass('dl-list__item--selected');
 
-            $target
-                .parent()
-                .addClass('dl-list__item--selected');
-        }
-    });
+        $item.addClass('dl-list__item--selected');
+    };
+
 
     $('.icon-list').on('click', function () {
         $('.main-container').addClass('l-offscreen--open-left');
@@ -41,4 +39,18 @@ $(function(){
     var contentHeight = $(window).height() - $('.header-container').height() -1;
     $('.left-sidebar').height(contentHeight);
     $('.main-content').height(contentHeight);
+
+
+    function showDemo(demoId){
+        sideBar.selectItem(demoId);
+    }
+
+    var sideBar = new SideBar();
+
+    var router = Router({
+        '/demos/:demoId': showDemo
+    });
+
+    router.init("demos/report-basic-render");
+
 });
