5f5251fc4ff61e649d6a68bc7bd61a33f1c6f870
drwebmaker <valeriy.abornev@globallogic.com>
2016-01-21 10:12:48 +0200

    #4 provide adaptive layout

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 /**
  * Created by valeriy.abornyev on 1/19/2016.
  */
-var sidebarHeight = $(window).height() - $('.header-container').height();
-$('.left-sidebar').height(sidebarHeight);
-$('.main-content').height(sidebarHeight);
+var contentHeight = $(window).height() - $('.header-container').height() -1;
+$('.left-sidebar').height(contentHeight);
+$('.main-content').height(contentHeight);
+
+function openList() {
+    $('.main-container').addClass('l-offscreen--open-left')
+}
+function closeList() {
+    $('.main-container').removeClass('l-offscreen--open-left')
+}
