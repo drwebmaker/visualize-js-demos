9047bcd8be222589e9df2878054a15bf6f7f3437
nesterone <igornester@gmail.com>
2016-01-21 15:46:43 +0200

    -add ability to select items on list -move inline js to file

--- js/main.js
+++ js/main.js
@@ -1,13 +1,44 @@
 /**
  * Created by valeriy.abornyev on 1/19/2016.
  */
-var contentHeight = $(window).height() - $('.header-container').height() -1;
-$('.left-sidebar').height(contentHeight);
-$('.main-content').height(contentHeight);
 
-function openList() {
-    $('.main-container').addClass('l-offscreen--open-left')
-}
-function closeList() {
-    $('.main-container').removeClass('l-offscreen--open-left')
-}
+$(function(){
+
+    'use strict';
+
+    function showDemo(demoId){
+        console.log(demoId);
+    }
+
+    var router = Router({
+        '/demos/:demoId': showDemo
+    });
+
+    router.init("demos/report-basic-render");
+
+    $('.dl-list').on('click', function(evt){
+        var $target = $(evt.target);
+        if ($target.is('a')){
+
+            $(this)
+                .find('.dl-list__item--selected')
+                .removeClass('dl-list__item--selected');
+
+            $target
+                .parent()
+                .addClass('dl-list__item--selected');
+        }
+    });
+
+    $('.icon-list').on('click', function () {
+        $('.main-container').addClass('l-offscreen--open-left');
+    });
+
+    $('.icon-cross').on('click', function () {
+        $('.main-container').removeClass('l-offscreen--open-left');
+    });
+
+    var contentHeight = $(window).height() - $('.header-container').height() -1;
+    $('.left-sidebar').height(contentHeight);
+    $('.main-content').height(contentHeight);
+});
